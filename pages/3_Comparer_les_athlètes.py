b4df326b473f27a82cfcb9f218f0169f494f49f2
Axelinhooo <axelcourtial@gmail.com>
2024-09-24 12:10:26 -0400

    graph en ligne pour comparer

--- pages/3_Comparer_les_athlètes.py
+++ pages/3_Comparer_les_athlètes.py
@@ -9,7 +9,7 @@ from streamlit_echarts import st_echarts
 from database.DatabaseManager import DatabaseManager
 
 
-def create_radar(filtered_datasets):
+def create_radar_athletes(filtered_datasets):
     # Définir le nombre maximal de sauts pour l'axe radar
     max_jumps = max([data.shape[0] for data in filtered_datasets]) + 1
     # arrondir au nombre divisible par 5 supérieur
@@ -68,10 +68,70 @@ def create_radar(filtered_datasets):
     else:
         st.write("Désolé, il n'y a pas de données à afficher.")
 
+def create_radar_types(filtered_datasets):
+    # Définir le nombre maximal de sauts pour l'axe radar
+    max_jumps = max([data.shape[0] for data in filtered_datasets]) + 1
+    # arrondir au nombre divisible par 5 supérieur
+    for i in range(1, 6):
+        if max_jumps % 5 == 0:
+            break
+        max_jumps += 1
+    
+    # Définir une liste de couleurs fixes
+    colors = ['#5470c6', '#91cc75', '#fac858', '#ee6666', '#73c0de', '#8c564b', '#64e572', '#fd8664', '#8a9899', '#e4c2d5']
+    
+    option = {
+        "legend": {"data": []},
+        "tooltip": {
+            "trigger": "item",
+        },
+        "radar": {
+            "indicator": [
+                {"name": "Total de sauts", "max": max_jumps, "axisLabel": {"show": True}},
+                {"name": "Taux de réussite (%)", "max": 100, "min": 0, "axisLabel": {"show": True}},
+                {"name": "Vitesse angulaire max (tours/s)", "max": 6, "axisLabel": {"show": True}},
+                {"name": "Durée de saut(s)", "max": 1, "min": 0, "axisLabel": {"show": True}},
+                {"name": "Sauts par entraînement", "max": 50, "min": 0, "axisLabel": {"show": True}},
+            ]
+        },
+        "series": [],
+    }
+
+    for index, data in enumerate(filtered_datasets):
+        if data.shape[0] > 0:
+            jump_type = str(data["jump_type"].unique()[0])
+            option["legend"]["data"].append(jump_type)
+            option["series"].append(
+                {
+                    "name": jump_type,
+                    "type": "radar",
+                    "itemStyle": {"color": colors[index % len(colors)]},  # Attribuer une couleur fixe
+                    "lineStyle": {"color": colors[index % len(colors)]},  # Assurer que la ligne a la même couleur
+                    "data": [
+                        {
+                            "value": [
+                                int(data.shape[0]),
+                                round(float(data["jump_success"].mean()), 1) * 100,
+                                round(float(data["jump_max_speed"].mean()), 1),
+                                round(float(data["jump_length"].mean()), 2),
+                                round(int(data.shape[0]) / int(data["training_date"].nunique()), 1),
+                            ],
+                            "name": jump_type,
+                        }
+                    ],
+                }
+            )
+
+    if option["series"]:
+        st_echarts(option, height="500px")
+    
 
 def create_line_chart(filtered_datasets):
     # Préparer les données pour le graphique en ligne
     line_data = []
+    # Mettre la colonne 'training_date' au format datetime
+    filtered_datasets = [data.assign(training_date=pd.to_datetime(data["training_date"])) for data in filtered_datasets]
+
     for data in filtered_datasets:
         if data.shape[0] > 0:
             data["month"] = data["training_date"].dt.to_period("M").astype(str)
@@ -139,12 +199,15 @@ if "logged_in" in st.session_state:
         for jump in jumps:
             filtered_jump = jump[
                 (jump["jump_type"] == selected_jump_type)
-                & (jump["jump_rotations"] == selected_jump_rotations)
+                & 
+                (jump["jump_rotations"] == selected_jump_rotations)
             ]
             filtered_datasets.append(filtered_jump)
-
-        create_radar(filtered_datasets)
         
+        create_radar_athletes(filtered_datasets)
+        create_line_chart(filtered_datasets)
+        # create_radar_types(filtered_datasets)
+
     else:
         st.error("Vous devez être connecté pour accéder à cette page.")
         st.stop()
